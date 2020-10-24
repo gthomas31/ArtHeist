406ed8e3fca6e2696bd709425e7e10b13db501db
ramizarif <72959302+ramizarif@users.noreply.github.com>
2020-10-24 14:24:06 -0400

    First main menu test
    working on connecting the menu to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+#import menu
 
 # Initialize pygame
 pygame.init()
@@ -10,6 +11,8 @@ screen = pygame.display.set_mode((1200,800))
 pygame.display.set_caption("Art Heist")
 icon = pygame.image.load('AHLogo.png')
 pygame.display.set_icon(icon)
+
+#play_button, tutorial_button, credit_button, practice_button = activemenu()
 
 # Player
 playerImg = pygame.image.load('GreenNinjaCharacter.png')
@@ -32,61 +35,61 @@ def enemy(x, y):
     screen.blit(enemyImg, (x, y))
 
 # Game Loop
-running = True
-while running:
+    running = True
+    while running:
 
-    # RGB (Red, Green, Blue)
-    screen.fill((254, 254, 254))
-    
-    playerX += playerX_change
-    enemyX += enemyX_change
+        # RGB (Red, Green, Blue)
+        screen.fill((254, 254, 254))
+        
+        playerX += playerX_change
+        enemyX += enemyX_change
 
-    # Boundaries for both characters
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 1140:
-        playerX = 1140
+        # Boundaries for both characters
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 1140:
+            playerX = 1140
 
-    if playerY <= 0:
-        playerY = 0
-    elif playerY >= 680:
-        playerY = 680 
+        if playerY <= 0:
+            playerY = 0
+        elif playerY >= 680:
+            playerY = 680 
 
-    if enemyX <= 700:
-        enemyX = 700
-    elif enemyX >= 1100:
-        enemyX = 1100
+        if enemyX <= 700:
+            enemyX = 700
+        elif enemyX >= 1100:
+            enemyX = 1100
 
-    if enemyY <= 0:
-        enemyY = 0
-    elif enemyY >= 680:
-        playerY = 680
+        if enemyY <= 0:
+            enemyY = 0
+        elif enemyY >= 680:
+            playerY = 680
 
-    # Enemy movement
-    if enemyX == 1100:
-        enemyX_change = -0.5
-    if enemyX == 700:
-        enemyX_change = 0.5
+        # Enemy movement
+        if enemyX == 1100:
+            enemyX_change = -0.5
+        if enemyX == 700:
+            enemyX_change = 0.5
 
-    # Update entity locations
-    player(playerX, playerY)
-    enemy(enemyX, enemyY)
-    pygame.display.update()
+        # Update entity locations
+        player(playerX, playerY)
+        enemy(enemyX, enemyY)
+        pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-                       
-        # Checks keystroke for left or right
-        # pygame.mouse.set_pos(400, 400)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -2
-                print("Left arrow")
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 2
-                print("Right arrow")
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
-                print("Released")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                        
+            # Checks keystroke for left or right
+            # pygame.mouse.set_pos(400, 400)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -2
+                    print("Left arrow")
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 2
+                    print("Right arrow")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+                    print("Released")
