fa5b69df191478b88b6d55d8cea5406f39f98b52
ramizarif <72959302+ramizarif@users.noreply.github.com>
2020-10-24 03:50:40 -0400

    Ramiz Menu
    Menu with drawn invisible buttons. Buttons do not activate fully yet.

---
+++ test.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Art Heist")
+    #INSERT WINDOW LOGO
+    #icon = pygame.image.load()
+    #pygame.display.set_icon(icon)
+s = pygame.Surface((400, 400))
+s.set_alpha(100)
+s.fill((255,255,255))
+win.blit(s, (200, 50))
+print(pygame.Surface.get_bounding_rect(s))
+pygame.display.update()
+
+#print(x.bottomleft[0])
+
+    #run until closed
+running = True 
+while running: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            running = False
+
+pygame.display.update()
+
+
